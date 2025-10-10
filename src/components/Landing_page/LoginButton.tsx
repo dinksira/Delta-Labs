850612c85825e808e567f1bf40e30694417b8210
DinkTutor <dinksiraelisa@gmail.com>
2025-10-10 18:00:53 +0300

    Add LoginButton component with Poppins font styling

---
+++ src/components/Landing_page/LoginButton.tsx
@@ -0,0 +1,19 @@
+// src/components/Landing_page/LoginButton.tsx
+'use client';
+
+export default function LoginButton() {
+  return (
+    <button
+      className="
+        w-[129px] h-[47px]
+        border border-white
+        rounded-full
+        text-white
+        text-[18px] font-poppins font-medium
+        bg-transparent
+      "
+    >
+      Login
+    </button>
+  );
+}
