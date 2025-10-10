686fe40fabea0416dc8a525ce86f84fad08019ba
DinkTutor <dinksiraelisa@gmail.com>
2025-10-10 18:01:11 +0300

    Add SignupButton component matching LoginButton style

---
+++ src/components/Landing_page/SignupButton.tsx
@@ -0,0 +1,19 @@
+// src/components/Landing_page/SignupButton.tsx
+'use client';
+
+export default function SignupButton() {
+  return (
+    <button
+      className="
+        w-[129px] h-[47px]
+        border border-white
+        rounded-full
+        text-white
+        text-[18px] font-poppins font-medium
+        bg-transparent
+      "
+    >
+      Sign Up
+    </button>
+  );
+}
